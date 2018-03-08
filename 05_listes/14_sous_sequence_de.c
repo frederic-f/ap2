a27d6e252aac1b94cf0bfc8712fad7d934d0d8ff
= <pv8@me.com>
2018-03-08 18:28:48 +0100

    sous_seq_recu

--- 05_listes/14_sous_sequence_de.c
+++ 05_listes/14_sous_sequence_de.c
@@ -6,7 +6,24 @@
 
 bool sous_seq_recu (liste L1, liste L2)
 {
-    return (true) ;
+    if (est_vide (L1))
+    {
+        return true ;
+    }
+
+    if (est_vide (L2))
+    {
+        return false ;
+    }
+
+    if ((prem (L1) == prem (L2))) // si le premier elément de chaque liste est commun
+    {
+        return (sous_seq_recu (reste (L1), reste (L2))) ; // on retire l'élement de L1, et continue avec reste de L2
+    }
+    else
+    {
+        return (sous_seq_recu (L1, reste (L2))) ; // otherwise we do not touch L1, and go with the next element of L2
+    }
 }
 
 bool sous_seq_iter (liste L1, liste L2)
@@ -14,7 +31,7 @@ bool sous_seq_iter (liste L1, liste L2)
     return (true) ;
 }
 
-void test_sous_seq_param (int a, liste L)
+void test_sous_seq_param (liste L1, liste L2)
 {
     printf("\n") ;
     // test de la version récursive
@@ -40,7 +57,7 @@ void test_sous_seq_param (int a, liste L)
 
 void test_sous_seq ()
 {
-    liste L, M, N, O ;
+    liste L, M, N, O, P ;
 
     L = l_vide () ;
 
@@ -55,9 +72,13 @@ void test_sous_seq ()
     test_sous_seq_param (N, M) ;
     test_sous_seq_param (M, N) ;
 
-    O = cons (1, cons (180, M))
+    O = cons (1, cons (180, M)) ;
 
     test_sous_seq_param (M, O) ;
+
+    P = cons (4, M) ;
+
+    test_sous_seq_param (P, O) ;
 
 }
 
